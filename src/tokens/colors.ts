697b19f21c31768964683e21cf52952f6759a3c9
julianosbento <juliano.bento@magnetis.com.br>
2021-12-28 12:37:42 -0300

    refactor: change default to named export

--- src/tokens/colors.ts
+++ src/tokens/colors.ts
@@ -1,9 +1,7 @@
-const colors = {
+export const colors = {
   error: '#BB2124',
   primary: '#4F7EFF',
   secondary: '#FFFFFF',
   success: '#22BB33',
   warning: '#F0AD4E',
 };
-
-export default colors;
